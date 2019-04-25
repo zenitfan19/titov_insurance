b57b37b8f7699105e59f1dbdbe6a186e1de94903
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-04-25 12:49:23 +0300

    fixes in desktop

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -16,5 +16,55 @@ $(document).ready(function () {
       });
       $(".news-block-item.wide.video").on("mouseleave", function(){
         $(this).find('.news-block-item-background > video').get(0).pause();        
-      });     
+      });  
+      
+      $(document).on('click', '.hamburger', function () {       
+        $(document).find('.header-mobile-menu').addClass('active fadeInDown animated');
+        if ($(this).hasClass('is-active')) {
+              $(document).find('.header-mobile-menu').removeClass('fadeInDown');
+              $(document).find('.header-mobile-menu').addClass('fadeOutUp');
+        } else {
+              $(document).find('.header-mobile-menu').removeClass('fadeOutUp');
+        }
+        $(this).toggleClass('is-active');
+      });
+      $(document).on('change', '#what-to-insure-switcher', function () {       
+        if($('#what-to-insure-switcher').prop('checked')) {
+          $('.insure-cats').css('display', 'none');
+          $('.insure-cats.additional').css('display', 'grid');
+        } else {
+          $('.insure-cats').css('display', 'grid');
+          $('.insure-cats.additional').css('display', 'none');
+        }
+      });
+      $(document).on('change', '#insurance-description-event', function () {       
+        if($('#insurance-description-event').prop('checked')) {
+          $('.insurance-description-content.event').css('display', 'none');
+          $('.insurance-description-content.event.additional').css('display', 'flex');
+        } else {
+          $('.insurance-description-content.event').css('display', 'flex');
+          $('.insurance-description-content.event.additional').css('display', 'none');
+        }
+      });
+      $(document).on('change', '#insurance-description-type', function () {       
+        if($('#insurance-description-type').prop('checked')) {
+          $('.insurance-description-content.type').css('display', 'none');
+          $('.insurance-description-content.type.additional').css('display', 'flex');
+        } else {
+          $('.insurance-description-content.type').css('display', 'flex');
+          $('.insurance-description-content.type.additional').css('display', 'none');
+        }
+      });
+      $(".useful-articles-content-item__link").on("mouseover", function(){
+        if($(this).find('img').hasClass('animated fadeInRight')) {
+          $(this).find('img').removeClass('animated fadeInRight');
+        }
+        $(this).find('img').addClass('animated fadeOutRight');                        
+      });
+      $(".useful-articles-content-item__link").on("mouseleave", function(){
+        if($(this).find('img').hasClass('animated fadeOutRight')) {
+          $(this).find('img').removeClass('animated fadeOutRight');
+        }
+        $(this).find('img').addClass('animated fadeInRight');        
+      }); 
 });
