6e2d844d6bae93a7a813f012fa8dc8152c2d24cc
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-04-26 08:09:22 +0300

    touch fix3

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -32,7 +32,9 @@ $(document).ready(function () {
         $(this).find('.news-block-item-date').css('color', '#fff');       
       });
       $(".news-block-item").not('.video').on("touchstart", function(){
-        $(this).find('.news-block-item-text').slideDown();        
+        $(this).find('.news-block-item-text').slideDown();
+        $('.news-block-item').css('background-color', 'unset'); 
+        $('.news-block-item').find('.news-block-item-date').css('color', '#fff');
         if($(window).width() < 787) {
         $(this).css('background-color', '#fff');
         $(this).find('.news-block-item-date').css('color', '#111');
